8b37dde2cf64f0f6617470b85e607225d92a89ea
PallaviAnde01 <pallaviande12393@gmail.com>
2022-08-11 16:48:50 +0530

    SBWCR-11:Implementation of save method in ServiceImpl

--- src/main/java/com/PallaviAnde/Service/EmplyoeeServiceImpl.java
+++ src/main/java/com/PallaviAnde/Service/EmplyoeeServiceImpl.java
@@ -1,5 +1,19 @@
 package com.PallaviAnde.Service;
 
-public class EmplyoeeServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.PallaviAnde.Model.Emplyoee;
+import com.PallaviAnde.Repository.EmplyoeeDao;
+
+@Service
+public class EmplyoeeServiceImpl implements EmplyoeeService{
+	@Autowired
+	private EmplyoeeDao emplyoeeDao;
+
+	@Override
+	public int saveData(Emplyoee emplyoee) {
+		int id = emplyoeeDao.saveData(emplyoee);
+		return id;
+	}
 
 }
