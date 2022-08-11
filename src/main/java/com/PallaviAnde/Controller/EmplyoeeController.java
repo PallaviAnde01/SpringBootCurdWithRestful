c05ea39ec77523f2bc26eca4221ce791e884490b
PallaviAnde01 <pallaviande12393@gmail.com>
2022-08-11 16:58:57 +0530

    SBWCR-12:Implementation of save method in Controller class

--- src/main/java/com/PallaviAnde/Controller/EmplyoeeController.java
+++ src/main/java/com/PallaviAnde/Controller/EmplyoeeController.java
@@ -1,5 +1,28 @@
 package com.PallaviAnde.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.PallaviAnde.Model.Emplyoee;
+import com.PallaviAnde.Service.EmplyoeeService;
+
+@RestController
 public class EmplyoeeController {
 
+	@Autowired
+	private EmplyoeeService emplyoeeService;
+	
+	@PostMapping(value="/savedata",consumes="application/json")
+	public ResponseEntity<String> saveData(@RequestBody Emplyoee emplyoee) {
+		int data = emplyoeeService.saveData(emplyoee);
+		String msg="Hii "+emplyoee.getName()+" your id is "+data;
+		
+		return new ResponseEntity<String>(msg,HttpStatus.CREATED);
+		}
+	
+	
 }
