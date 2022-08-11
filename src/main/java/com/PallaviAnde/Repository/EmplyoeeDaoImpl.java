8e3ba5298ee67944d4f219b13a2350a7802d9882
PallaviAnde01 <pallaviande12393@gmail.com>
2022-08-11 16:39:01 +0530

    SBWCR-9:Implementation of save method in DaoImpl class

--- src/main/java/com/PallaviAnde/Repository/EmplyoeeDaoImpl.java
+++ src/main/java/com/PallaviAnde/Repository/EmplyoeeDaoImpl.java
@@ -1,5 +1,25 @@
 package com.PallaviAnde.Repository;
 
-public class EmplyoeeDaoImpl {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.PallaviAnde.Model.Emplyoee;
+
+@Repository
+public class EmplyoeeDaoImpl implements EmplyoeeDao{
+
+	@Autowired
+	private SessionFactory sf;
+	
+	@Override
+	public int saveData(Emplyoee emplyoee) {
+		Session session = sf.openSession();
+		session.beginTransaction();
+		 int id = (int) session.save(emplyoee);
+		session.getTransaction().commit();
+		return id;
+	}
 
 }
