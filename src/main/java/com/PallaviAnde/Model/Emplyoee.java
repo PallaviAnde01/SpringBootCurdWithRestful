c6e36159ff77cefa10a0fac42e299b41cad4aec9
PallaviAnde01 <pallaviande12393@gmail.com>
2022-08-11 15:35:52 +0530

    SBWCR-2: Create Entity class

--- src/main/java/com/PallaviAnde/Model/Emplyoee.java
+++ src/main/java/com/PallaviAnde/Model/Emplyoee.java
@@ -1,5 +1,74 @@
 package com.PallaviAnde.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Emplyoee {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String username;
+	private String password;
+	private String name;
+	private String address;
+	private String email;
+	private Integer age;
+	private Double salary;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Emplyoee [id=" + id + ", username=" + username + ", password=" + password + ", name=" + name
+				+ ", address=" + address + ", email=" + email + ", age=" + age + ", salary=" + salary + "]";
+	}
 }
